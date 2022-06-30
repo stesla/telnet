b55c11258c67f64c41e1bb46264be8285ab2e92f
Samuel Tesla <samuel.tesla@gmail.com>
2022-06-30 03:29:35 -0500

    refactor reader to use state pattern

--- reader.go
+++ reader.go
@@ -5,17 +5,21 @@ import "io"
 const readerCapacity = 1024
 
 func NewReader(r io.Reader) io.Reader {
-	return &reader{
+	result := &reader{
 		in:  r,
 		buf: make([]byte, readerCapacity),
 	}
+	result.state = result.decodeByte
+	return result
 }
 
 type reader struct {
-	in      io.Reader
-	b, buf  []byte
-	cr, iac bool
+	in     io.Reader
+	b, buf []byte
+	state  readerState
 }
+
+type readerState func(byte) (readerState, byte, bool)
 
 func (r *reader) Read(p []byte) (n int, err error) {
 	if len(r.b) == 0 {
@@ -24,32 +28,45 @@ func (r *reader) Read(p []byte) (n int, err error) {
 		r.b = r.buf[:n]
 	}
 	for len(r.b) > 0 && n < len(p) {
-		c := r.b[0]
+		var c byte
+		var ok bool
+		r.state, c, ok = r.state(r.b[0])
 		r.b = r.b[1:]
-		if r.iac {
-			r.iac = false
-			if c == IAC {
-				p[n] = IAC
-				n++
-			}
-			continue
-		} else if r.cr {
-			r.cr = false
-			if c == 0 {
-				p[n] = '\r'
-				n++
-				continue
-			}
-		}
-		switch c {
-		case IAC:
-			r.iac = true
-		case '\r':
-			r.cr = true
-		default:
+		if ok {
 			p[n] = c
 			n++
 		}
 	}
 	return
+}
+
+func (r *reader) decodeByte(c byte) (readerState, byte, bool) {
+	switch c {
+	case IAC:
+		return r.decodeCommand, c, false
+	case '\r':
+		return r.decodeCarriageReturn, c, false
+	default:
+		return r.decodeByte, c, true
+	}
+}
+
+func (r *reader) decodeCommand(c byte) (readerState, byte, bool) {
+	switch c {
+	case IAC:
+		return r.decodeByte, c, true
+	default:
+		return r.decodeByte, c, false
+	}
+}
+
+func (r *reader) decodeCarriageReturn(c byte) (readerState, byte, bool) {
+	switch c {
+	case '\x00':
+		return r.decodeByte, '\r', true
+	case '\r':
+		return r.decodeByte, c, false
+	default:
+		return r.decodeByte, c, true
+	}
 }
