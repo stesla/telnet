a53e6b69b45f4c0678c9a166db7d985782c9abc9
Admin <admin@localhost>
2022-07-04 19:46:53 -0500

    WIP Charset option

---
+++ charset_test.go
@@ -0,0 +1,49 @@
+package telnet
+
+import (
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"golang.org/x/text/encoding/unicode"
+)
+
+func TestEnableDisable(t *testing.T) {
+	var h OptionHandler = &CharsetOption{}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	conn := NewMockConn(ctrl)
+	h.Enable(conn)
+	h.Disable(conn)
+}
+
+func TestAcceptASCII(t *testing.T) {
+	var h OptionHandler = &CharsetOption{}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	conn := NewMockConn(ctrl)
+
+	expected := []byte{IAC, SB, Charset, charsetAccepted}
+	expected = append(expected, "US-ASCII"...)
+	expected = append(expected, IAC, SE)
+	conn.EXPECT().Send(expected)
+	conn.EXPECT().SetEncoding(ASCII)
+	data := []byte{charsetRequest}
+	data = append(data, "[TTABLE]\x01;ISO-8859-1;US-ASCII;CP437"...)
+	h.Subnegotiation(conn, data)
+}
+
+func TestAcceptUTF8(t *testing.T) {
+	var h OptionHandler = &CharsetOption{}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	conn := NewMockConn(ctrl)
+
+	expected := []byte{IAC, SB, Charset, charsetAccepted}
+	expected = append(expected, "UTF-8"...)
+	expected = append(expected, IAC, SE)
+	conn.EXPECT().Send(expected)
+	conn.EXPECT().SetEncoding(unicode.UTF8)
+	data := []byte{charsetRequest}
+	data = append(data, "[TTABLE]\x01;UTF-8;ISO-8859-1;US-ASCII;CP437"...)
+	h.Subnegotiation(conn, data)
+}
