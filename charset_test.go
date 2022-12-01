021e3099d14e6a3bffd8665856b7ec65fbcba869
Samuel Tesla <samuel.tesla@gmail.com>
2022-11-30 20:17:52 -0600

    DRY up tests

--- charset_test.go
+++ charset_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 )
 
@@ -27,32 +28,26 @@ func TestRejectIfNotEnabled(t *testing.T) {
 	})
 }
 
-func TestAcceptASCII(t *testing.T) {
-	withCharsetAndConn(t, func(h OptionHandler, conn *MockConn) {
-		h.EnableForUs(conn)
-
-		expected := []byte{IAC, SB, Charset, charsetAccepted}
-		expected = append(expected, "US-ASCII"...)
-		expected = append(expected, IAC, SE)
-		conn.EXPECT().Send(expected)
-		conn.EXPECT().SetEncoding(ASCII)
-		data := []byte{charsetRequest}
-		data = append(data, "[TTABLE]\x01;ISO-8859-1;US-ASCII;CP437"...)
-		h.Subnegotiation(conn, data)
-	})
-}
-
-func TestAcceptUTF8(t *testing.T) {
-	withCharsetAndConn(t, func(h OptionHandler, conn *MockConn) {
-		h.EnableForUs(conn)
-
-		expected := []byte{IAC, SB, Charset, charsetAccepted}
-		expected = append(expected, "UTF-8"...)
-		expected = append(expected, IAC, SE)
-		conn.EXPECT().Send(expected)
-		conn.EXPECT().SetEncoding(unicode.UTF8)
-		data := []byte{charsetRequest}
-		data = append(data, "[TTABLE]\x01;UTF-8;ISO-8859-1;US-ASCII;CP437"...)
-		h.Subnegotiation(conn, data)
-	})
+func TestAcceptEncoding(t *testing.T) {
+	var tests = []struct {
+		encoding           encoding.Encoding
+		encodingName       string
+		subnegotiationData string
+	}{
+		{ASCII, "US-ASCII", "[TTABLE]\x01;ISO-8859-1;US-ASCII;CP437"},
+		{unicode.UTF8, "UTF-8", "[TTABLE]\x01;UTF-8;ISO-8859-1;US-ASCII;CP437"},
+	}
+	for _, test := range tests {
+		withCharsetAndConn(t, func(h OptionHandler, conn *MockConn) {
+			h.EnableForUs(conn)
+			expected := []byte{IAC, SB, Charset, charsetAccepted}
+			expected = append(expected, test.encodingName...)
+			expected = append(expected, IAC, SE)
+			conn.EXPECT().Send(expected)
+			conn.EXPECT().SetEncoding(test.encoding)
+			data := []byte{charsetRequest}
+			data = append(data, test.subnegotiationData...)
+			h.Subnegotiation(conn, data)
+		})
+	}
 }
